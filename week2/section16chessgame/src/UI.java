6f59c26fda1f3df632257f74b897a33777bb10d9
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-04-01 20:50:51 +0100

    Swiching player each turn

--- week2/section16chessgame/src/UI.java
+++ week2/section16chessgame/src/UI.java
@@ -1,3 +1,4 @@
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -41,6 +42,13 @@ public class UI {
             throw new InputMismatchException("Error reading chess position. Valid values are from a1 to h8.");
         }
 
+    }
+
+    public static void printMatch(ChessMatch chessMatch){
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
     }
     public static void printBoard(ChessPiece[][] chessPiece){
         for (int i = 0; i < chessPiece.length; i++){
