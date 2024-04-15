9ef34e0e729896415a58c28ed242b075fad4482b
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-04-15 16:10:38 -0300

    feat: Jpa repository, dependency injection, database seeding

---
+++ week5/course/src/main/java/com/pbcompass/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.pbcompass.course.repositories;
+
+import com.pbcompass.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
