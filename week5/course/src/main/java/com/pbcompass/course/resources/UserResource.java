34ff478fe3f22e3ce87dd8c4bc1553780ad7a39b
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-04-15 16:27:37 -0300

    feat: Service layer, component registration

--- week5/course/src/main/java/com/pbcompass/course/resources/UserResource.java
+++ week5/course/src/main/java/com/pbcompass/course/resources/UserResource.java
@@ -1,19 +1,38 @@
 package com.pbcompass.course.resources;
 
 import com.pbcompass.course.entities.User;
+import com.pbcompass.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(1L, "Maria", "maria@gmail.com", "9999999", "123456");
-        return ResponseEntity.ok().body(user);
+    private UserService userService;
+
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = new ArrayList<>();
+        users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User ubj = userService.findById(id);
+        return ResponseEntity.ok().body(ubj);
+    }
 }
