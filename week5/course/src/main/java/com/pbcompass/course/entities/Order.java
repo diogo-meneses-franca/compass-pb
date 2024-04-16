14593bac590fde00422786bdd4c4e970fb577946
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-04-16 19:01:47 -0300

    Payment one-to-one association

--- week5/course/src/main/java/com/pbcompass/course/entities/Order.java
+++ week5/course/src/main/java/com/pbcompass/course/entities/Order.java
@@ -32,6 +32,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> orderItems = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {}
 
     public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
@@ -41,6 +44,13 @@ public class Order implements Serializable {
         this.client = client;
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
     public Set<OrderItem> getOrderItems() {
         return orderItems;
     }
