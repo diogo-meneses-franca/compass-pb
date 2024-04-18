d69d3a8ee29547042260003bf9a2a5b390f88773
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-04-18 16:03:07 -0300

    feat: starting project wokshopmongodb

---
+++ week5/workshopmongodb/src/test/java/br/com/pbcompass/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pbcompass.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
