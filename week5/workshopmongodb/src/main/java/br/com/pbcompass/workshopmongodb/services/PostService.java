9bd420767bc74a5b955513345a75c5dca0bee8ce
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-04-21 15:09:06 -0300

    feat: method findbytitle with @query

--- week5/workshopmongodb/src/main/java/br/com/pbcompass/workshopmongodb/services/PostService.java
+++ week5/workshopmongodb/src/main/java/br/com/pbcompass/workshopmongodb/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
     }
 
     public List<Post> findByTitleContaining(String text){
-        return postRepository.findByTitleContainingIgnoreCase(text);
+        return postRepository.findByTitle(text);
     }
 }
