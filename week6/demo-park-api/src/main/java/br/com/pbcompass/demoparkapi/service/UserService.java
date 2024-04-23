77c9470f483940bc771680e7635496bea7fb25ce
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-04-23 17:33:38 -0300

    feat: method findbyid for users

--- week6/demo-park-api/src/main/java/br/com/pbcompass/demoparkapi/service/UserService.java
+++ week6/demo-park-api/src/main/java/br/com/pbcompass/demoparkapi/service/UserService.java
@@ -2,6 +2,7 @@ package br.com.pbcompass.demoparkapi.service;
 
 import br.com.pbcompass.demoparkapi.entity.User;
 import br.com.pbcompass.demoparkapi.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +16,10 @@ public class UserService {
     @Transactional
     public User save(User user) {
         return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
     }
 }
