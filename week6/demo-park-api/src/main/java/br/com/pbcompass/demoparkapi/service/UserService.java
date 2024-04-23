70ecbc1dbc466406f0974a138feccc0eef8cb91f
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-04-23 17:18:58 -0300

    feat: save method on user class

--- week6/demo-park-api/src/main/java/br/com/pbcompass/demoparkapi/service/UserService.java
+++ week6/demo-park-api/src/main/java/br/com/pbcompass/demoparkapi/service/UserService.java
@@ -1,12 +1,19 @@
 package br.com.pbcompass.demoparkapi.service;
 
+import br.com.pbcompass.demoparkapi.entity.User;
 import br.com.pbcompass.demoparkapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
