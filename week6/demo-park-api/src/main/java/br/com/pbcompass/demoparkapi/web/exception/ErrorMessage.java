5310fc2f828960e801b86aeff13266a40eca0893
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-04-25 13:29:40 -0300

    feat: class errormessage

---
+++ week6/demo-park-api/src/main/java/br/com/pbcompass/demoparkapi/web/exception/ErrorMessage.java
@@ -0,0 +1,47 @@
+package br.com.pbcompass.demoparkapi.web.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@ToString
+public class ErrorMessage {
+
+    private String path;
+    private String method;
+    private int status;
+    private String statusMessage;
+    private String message;
+    private Map<String, String> errors = new HashMap<>();
+
+    public ErrorMessage() {}
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusMessage = status.getReasonPhrase();
+        this.message = message;
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult result) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusMessage = status.getReasonPhrase();
+        this.message = message;
+        addErrors(result);
+    }
+
+    private void addErrors(BindingResult result) {
+        for (FieldError error : result.getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
