898fb1e2d8e3a4c1abbec06bd1a09e524bbfdcdf
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-05-02 14:19:12 -0300

    feat: class ParkingSpace

---
+++ week6/demo-park-api/src/main/java/br/com/pbcompass/demoparkapi/entity/ParkingSpace.java
@@ -0,0 +1,71 @@
+package br.com.pbcompass.demoparkapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "parking_space")
+@EntityListeners(AuditingEntityListener.class)
+public class ParkingSpace implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "code", nullable = false, unique = true, length = 4)
+    private String code;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ParkingSpaceStatus status;
+
+    @CreatedDate
+    @Column(name = "created")
+    private LocalDateTime created;
+
+    @LastModifiedDate
+    @Column(name = "updated")
+    private LocalDateTime updated;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    public enum ParkingSpaceStatus {
+        FREE,
+        OCCUPIED;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParkingSpace that = (ParkingSpace) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
